0290a4c5f286fde3afcdac978ab2052dc8d4c699
Anggit Muhamad Ginanjar <anggitmg.tkjb@gmail.com>
2017-11-07 01:33:00 +0700

    Add Select() method in testing file for fetching data from table as array struct by Anggit Muhamad Ginanjar

--- models/models_testing_sqlx.go
+++ models/models_testing_sqlx.go
@@ -1,3 +1,5 @@
+// testing 
+
 package main
 
 import (
@@ -6,6 +8,13 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/go-sql-driver/mysql"
 )
+
+// user login struct for fetching all data from table
+// it'll using to fetch data from as array struct
+type user_login struct {
+	User_id		string
+	User_name	string
+}
 
 func main() {
 	var db *sqlx.DB
@@ -17,6 +26,7 @@ func main() {
 		log.Println("MySQL Error:", err)
 	}
 
+	// single value with Queryx method
 	rows, _ := db.Queryx("SELECT user_id, user_name FROM user_login")
 
 	for rows.Next() {
@@ -24,10 +34,22 @@ func main() {
 		var user_name string
 
 		err = rows.Scan(&user_id, &user_name)
-		fmt.Println(user_id, user_name)
+		fmt.Println(user_id, user_name)	// print:	001 Administrator
+										//          002 Anggit Muhamad Ginanjar
 		if err != nil {
 			log.Println("MySQL Error:", err)
 		}
 	}
 
+	// multiple value within array struct using Get() method
+	// create array struct variable, the struct is reference from user_login struct
+	users := []user_login{}
+	// usgin Select() method for fetching all data in table as array struct
+	err = db.Select(&users, "SELECT user_id, user_name FROM user_login")
+	fmt.Println(users)	// print: [{001 Administrator}, {002, Anggit Muhamad Ginanjar}]
+	fmt.Println(users[1].User_name)		// print: Anggit Muhamad Ginanjar
+
+	if err != nil {
+		log.Println(err)
+	}
 }
