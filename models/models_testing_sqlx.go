f00dca6c698c59198d475b73d1b5a3bb6f893309
Anggit Muhamad Ginanjar <anggitmg.tkjb@gmail.com>
2017-11-07 00:14:57 +0700

    Testing SQLX using Queryx method was successfuly by Anggit Muhamad Ginanjar

--- models/models_testing_sqlx.go
+++ models/models_testing_sqlx.go
@@ -11,24 +11,23 @@ func main() {
 	var db *sqlx.DB
 	var err error
 
-	db, err = sqlx.Connect("mysql", "dc:P4sswordDC@tcp(127.0.0.1:3306)/stockapps")
+	db, err = sqlx.Connect("mysql", "dc:IniP4ssword@tcp(127.0.0.1:3306)/stockapps")
 
 	if err != nil {
 		log.Println("MySQL Error:", err)
 	}
 
-	rows, _ := db.Query("SELECT FROM user_id, user_name user_login")
+	rows, _ := db.Queryx("SELECT user_id, user_name FROM user_login")
 
 	for rows.Next() {
-		var user_id, user_name string
-		err = rows.Scan(&user_id, user_name)
+		var user_id string
+		var user_name string
+
+		err = rows.Scan(&user_id, &user_name)
 		fmt.Println(user_id, user_name)
 		if err != nil {
 			log.Println("MySQL Error:", err)
 		}
 	}
 
-	if err != nil {
-		log.Println("MySQL Error:", err)
-	}
 }
