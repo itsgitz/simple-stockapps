38da7049b0fb042703a2bbf87f0db13ab612b1ff
Anggit Muhamad Ginanjar <anggitmg.tkjb@gmail.com>
2017-11-07 22:54:20 +0700

    Adding HTTP Middleware Handler (main.go) and Main Controller (main_controllers.go)

--- controllers/main_controllers.go
+++ controllers/main_controllers.go
@@ -1,9 +1,20 @@
 package controllers
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"html/template"
 )
 
-func main() {
-	fmt.Println("Hellow Controllers")
+type MainController struct {}
+
+func (this MainController) AppMainPage(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.New("").Delims("[[", "]]").ParseFiles("views/main.tpl")
+	if err != nil {
+		log.Println(err)
+	}
+	err = tpl.ExecuteTemplate(w, "main_layout", nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"log"
+	"time"
+	"net/http"
+	"github.com/urfave/negroni"
+	"github.com/gorilla/mux"
+	"_apps_/controllers"
+)
+
+func main() {
+	router := mux.NewRouter()
+	main_ctrl := new(controllers.MainController)
+	router.HandleFunc("/", main_ctrl.AppMainPage)
+	middleware := negroni.Classic()
+	middleware.Use(negroni.NewStatic(http.Dir("static")))
+	middleware.UseHandler(router)
+
+	HttpServer := &http.Server{
+		Addr:			":8080",
+		Handler:		middleware,
+		ReadTimeout:	time.Second * 10,
+		WriteTimeout:	time.Second * 10,
+	}
+	log.Println("[*] Web server is running on port :8080")
+	HttpError := HttpServer.ListenAndServe()
+
+	if HttpError != nil {
+		log.Println("HttpError:", HttpError)
+	}
+}
