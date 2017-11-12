b397608dab507c9452f5d969a9261f108760dd95
Anggit Muhamad Ginanjar <anggitmg.tkjb@gmail.com>
2017-11-12 11:45:31 +0700

    main_controllers.go: Adding AppLogin() function for login handler and connecting it to models package

--- controllers/main_controllers.go
+++ controllers/main_controllers.go
@@ -1,20 +1,52 @@
+// Controller package for Main Controller object
+// by: Anggit Muhamad Ginanjar
+//     STM NEGERI PEMBANGUNAN BANDUNG
 package controllers
 
 import (
 	"log"
 	"net/http"
 	"html/template"
+	"simple_stockapps/models"
 )
 
-type MainController struct {}
+type MainController struct {
+	AuthLoginMessage	string	`json:""`
+}
 
+// Main page controller
 func (this MainController) AppMainPage(w http.ResponseWriter, r *http.Request) {
-	tpl, err := template.New("").Delims("[[", "]]").ParseFiles("views/main.tpl")
-	if err != nil {
-		log.Println(err)
+	html_data := struct{
+		Title string
+	}{
+		Title: "Simple StockApps",
 	}
-	err = tpl.ExecuteTemplate(w, "main_layout", nil)
+
+	tpl_filename := "views/main.tpl"
+	tpl, err := template.New("").Delims("[[", "]]").ParseFiles(tpl_filename)
 	if err != nil {
-		log.Println(err)
+		log.Println("[!] ERROR:", err)
 	}
+
+	err = tpl.ExecuteTemplate(w, "main_layout", html_data)
+	if err != nil {
+		log.Println("[!] ERROR:", err)
+	}
+}
+
+// login process handler
+// custom login authentication
+// checking if username and password is exists (matching)
+func (this MainController) AppLogin(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()	// parsing form (data input)
+	log.Println(r.Form["username"])
+	log.Println(r.Form["password"])
+	username := r.Form["username"][0]
+	password := r.Form["password"][0]
+
+	// check username and password in table
+	// if exists then return true,
+	// else, return false
+	user_isExists := models.ModelsReadLogin(username, password) // print true / false
+	log.Println(user_isExists)
 }
