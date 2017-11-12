e09463242fbfdaef48ee132d991548f79974d2a9
Anggit Muhamad Ginanjar <anggitmg.tkjb@gmail.com>
2017-11-12 11:44:38 +0700

    main.go: Adding `/login` router

--- main.go
+++ main.go
@@ -1,31 +1,46 @@
+// Main package ...
+// by: Anggit Muhamad Ginanjar
+//     STM NEGERI PEMBANGUNAN BANDUNG
 package main
 
 import (
-	"log"
-	"time"
-	"net/http"
-	"github.com/urfave/negroni"
-	"github.com/gorilla/mux"
-	"simple_stockapps/controllers"
+	"log"	// log	package
+	"time"	// time package
+	"net/http"	// http for http server handling
+	"github.com/urfave/negroni"	// negroni HTTP Middleware
+	"github.com/gorilla/mux"	// gorilla router handler
+	"simple_stockapps/controllers"	// custom controllers
 )
 
+// main function
 func main() {
+	// router variable initialiation using mux gorilla package
 	router := mux.NewRouter()
+	// defining main controller object
 	main_ctrl := new(controllers.MainController)
-	router.HandleFunc("/", main_ctrl.AppMainPage)
-	middleware := negroni.Classic()
-	middleware.Use(negroni.NewStatic(http.Dir("static")))
-	middleware.UseHandler(router)
 
+	// defining router handler function
+	router.HandleFunc("/", main_ctrl.AppMainPage) // routing for "/" (root) handler
+	router.HandleFunc("/login", main_ctrl.AppLogin) // routing for "/login" handler
+
+	// defining http middleware using negroni method
+	middleware := negroni.Classic()
+	middleware.Use(negroni.NewStatic(http.Dir("static"))) // initialization static file
+	middleware.UseHandler(router) // using gorilla mux router inside negroni middleware
+
+	// initialization http server
 	HttpServer := &http.Server{
-		Addr:			":8080",
-		Handler:		middleware,
+		Addr:			":8080", // http port that used by web server
+		Handler:		middleware, // using handler as http middleware
 		ReadTimeout:	time.Second * 10,
 		WriteTimeout:	time.Second * 10,
 	}
+
+	// log will tell web server has already opening on port :8080
 	log.Println("[*] Web server is running on port :8080")
 	HttpError := HttpServer.ListenAndServe()
 
+	// catch the error log if any error outhere
 	if HttpError != nil {
 		log.Println("HttpError:", HttpError)
 	}
