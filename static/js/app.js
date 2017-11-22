b38a8a7aab6f4c807c0455fb0547c192e7256fd0
Anggit Muhamad Ginanjar <anggitmg.tkjb@gmail.com>
2017-11-23 00:09:06 +0700

    New JavaScript file

---
+++ static/js/app.js
@@ -0,0 +1,91 @@
+// jQuery.3.2.1
+$(document).ready(function() {
+	// login popup box function
+	appLoginHandler();
+	// table handler function
+	appTableHandler();
+});
+
+// Login popup box function
+function appLoginHandler() {
+	// login form variable element
+	var loginForm = $("form.app-login-form");
+	// login popup box
+	var loginPopupBox = $("div#app-login-popup");
+	// login button and close button
+	var signButton = $("button.app-sign-btn");
+	var signText = signButton.text();
+	var closeButton = $("button.app-close-btn");
+
+	// show login popup when sign button clicked
+	signButton.click(function() {
+		//loginPopupBox.css("display", "block");
+		loginPopupBox.show(500);
+	});
+	// close login popup
+	closeButton.click(function() {
+		loginPopupBox.fadeOut();
+	});
+
+	// if user has logged in, then sign button changes to logout button
+	// ajax will send request to AppLogout Handler
+	if (signText == "Logout") {
+		signButton.click(function() {
+			loginPopupBox.css("display", "none");
+			window.location = "/logout";
+		});
+	}
+
+	// login form for prevent default
+	loginForm.submit(function(e) {
+		e.preventDefault();
+		// username and password value
+		var usernameValue = $("input.app-username").val();
+		var passwordValue = $("input.app-password").val();
+
+		// submit while value not null
+		if (usernameValue && passwordValue) {
+		// these values will send to server (login controller) using AJAX method
+			$.ajax({
+				url: "/login",	// send data to login handler on server
+				async: true,
+				data: {
+					username: usernameValue,
+					password: passwordValue
+				},
+				success: function(jsonLoginDataAuth) {
+					// jsonLoginDataAuth --> JSON data that sended from login handler AppLogin
+					if (jsonLoginDataAuth.Message) {
+						window.location = jsonLoginDataAuth.Redirect_Url;
+					} else {
+						$("div.app-login-alert").html("<b>Incorrect username or password!</b>");
+					}
+				}
+			});
+			loginForm[0].reset(); // reset (clearing) the login form after submit
+		}
+	});
+}
+
+// appTableHandler for handling table items
+function appTableHandler() {
+	var ws = new WebSocket('ws://192.168.43.56:8080/ws');
+	ws.onopen = function() {
+		console.log("Connection Open");
+	}
+	ws.onerror = function(error) {
+		console.error('WebSocket' + error);
+	}
+	ws.onmessage = function(e) {
+		var msg = JSON.parse(e.data)
+			$("p.app-msg-box").html(msg.Pesan);
+		}
+		// button on click
+	$("button.app-send").click(function() {
+		var msg = $("input.message").val();
+		var json_msg = JSON.stringify({
+			Pesan: msg
+		});
+			ws.send(json_msg);
+	});
+}
