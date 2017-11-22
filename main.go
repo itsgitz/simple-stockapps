0c9c29d67b8a1e36f06784cb613eb616a18f72b8
Anggit Muhamad Ginanjar <anggitmg.tkjb@gmail.com>
2017-11-22 18:35:19 +0700

    Done adding router into websocket system

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	router.HandleFunc("/", main_ctrl.AppMainPage) // routing for "/" (root) handler
 	router.HandleFunc("/login", main_ctrl.AppLogin) // routing for "/login" handler
 	router.HandleFunc("/logout", main_ctrl.AppLogout) // routing for "/logout" handler
+	router.HandleFunc("/realtime", main_ctrl.AppWebSocket) // routing for web socket handler
 
 	// defining http middleware using negroni method
 	middleware := negroni.Classic()
